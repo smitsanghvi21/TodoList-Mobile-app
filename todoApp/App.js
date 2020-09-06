36274eecd04997bc60acbaae110333e496a83443
Smit Sanghvi <smitsanghvi@MacBook-Pro.local>
2020-09-06 21:41:48 +1200

    Fixes

--- todoApp/App.js
+++ todoApp/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <div>
-      hello worldd
-    </div>
-  )
-}
+    <View style={styles.container}>
+      <Text>hellow world</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
